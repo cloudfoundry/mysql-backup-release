6cf07baa425c115138c18f83e904ccedad221ef0
Chris Tarazi <ctarazi@pivotal.io>
2019-03-19 14:41:59 -0700

    Run streaming-mysql-backup-tool job under BPM
    * Remove the references to PIDfiles outside of BPM context
    
    Signed-off-by: James Palmer <jpalmer@pivotal.io>
    Signed-off-by: Kai Xiang <kxiang@pivotal.io>
    
    [#163524930]

--- src/streaming-mysql-backup-tool/main.go
+++ src/streaming-mysql-backup-tool/main.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
-	"strconv"
 
-	"code.cloudfoundry.org/lager"
-	"github.com/cloudfoundry-incubator/switchboard/api/middleware"
 	"streaming-mysql-backup-tool/api"
 	"streaming-mysql-backup-tool/collector"
 	"streaming-mysql-backup-tool/config"
+
+	"code.cloudfoundry.org/lager"
+	"github.com/cloudfoundry-incubator/switchboard/api/middleware"
 )
 
 func main() {
@@ -39,13 +38,6 @@ func main() {
 	}
 
 	mux.Handle("/backup", backupHandler)
-
-	pidfile, err := os.Create(config.PidFile)
-	if err != nil {
-		logger.Fatal("Failed to create a file", err)
-	}
-
-	ioutil.WriteFile(pidfile.Name(), []byte(strconv.Itoa(os.Getpid())), 0644)
 
 	logger.Info("Starting server with configuration", lager.Data{
 		"port": config.Port,
