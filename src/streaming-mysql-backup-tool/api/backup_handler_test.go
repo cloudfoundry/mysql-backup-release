64e69a8e0bd6b4496490fc05b790689b7ae286ce
Andrew Garner <garnera@vmware.com>
2022-12-01 21:18:07 -0600

    streaming-mysql-backup-tool: remove collector logic
    This mechanism has not been working for a long time now since the
    transition to pxc-release.  Delete the non-functional code.
    
    [#183599195](https://www.pivotaltracker.com/story/show/183599195)
    
    Co-authored-by: Andrew Garner <garnera@vmware.com>
    Co-authored-by: Kim Bassett <kbassett@vmware.com>

--- src/streaming-mysql-backup-tool/api/backup_handler_test.go
+++ src/streaming-mysql-backup-tool/api/backup_handler_test.go
@@ -6,17 +6,16 @@ import (
 	"os/exec"
 
 	"code.cloudfoundry.org/lager/lagertest"
-	. "github.com/cloudfoundry/streaming-mysql-backup-tool/api"
-	"github.com/cloudfoundry/streaming-mysql-backup-tool/api/apifakes"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	. "github.com/cloudfoundry/streaming-mysql-backup-tool/api"
 )
 
 var _ = Describe("BackupHandler", func() {
 	var (
 		command            *exec.Cmd
 		testLogger         *lagertest.TestLogger
-		fakeCollector      *apifakes.FakeCollector
 		backupHandler      *BackupHandler
 		fakeResponseWriter *httptest.ResponseRecorder
 		request            *http.Request
@@ -24,24 +23,20 @@ var _ = Describe("BackupHandler", func() {
 	)
 
 	BeforeEach(func() {
-		fakeCollector = new(apifakes.FakeCollector)
-
 		command = exec.Command("some-path")
-		testLogger = lagertest.NewTestLogger("collector-test")
+		testLogger = lagertest.NewTestLogger("handler-test")
 		backupHandler = &BackupHandler{
-			CommandGenerator:   func() *exec.Cmd { return command },
-			CollectorGenerator: func() Collector { return fakeCollector },
-			Logger:             testLogger,
+			CommandGenerator: func() *exec.Cmd { return command },
+			Logger:           testLogger,
 		}
 		fakeResponseWriter = httptest.NewRecorder()
 	})
 
-	It("executes the command while collecting information", func() {
+	It("executes the command", func() {
 		request, err = http.NewRequest("GET", "/backups", nil)
 		Expect(err).NotTo(HaveOccurred())
 
 		backupHandler.ServeHTTP(fakeResponseWriter, request)
-		Eventually(fakeCollector.StartCallCount).Should(Equal(1))
-		Eventually(fakeCollector.StopCallCount).Should(Equal(1))
+		Expect(fakeResponseWriter.Result().Header.Get(TrailerKey)).To(ContainSubstring(`"some-path": executable file not found`))
 	})
 })
