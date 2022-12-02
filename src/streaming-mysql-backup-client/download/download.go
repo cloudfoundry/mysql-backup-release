40faad294fa55fec6d8e58554bbe299c3ce13f90
Andrew Garner <garnera@vmware.com>
2022-12-02 08:51:08 -0600

    streaming-mysql-backup-client: use idiomatic go
    Rename receiver from "this" to be more idiomatic.
    
    [#183599195](https://www.pivotaltracker.com/story/show/183599195)

--- src/streaming-mysql-backup-client/download/download.go
+++ src/streaming-mysql-backup-client/download/download.go
@@ -61,7 +61,7 @@ func (tr *trackingReader) Read(p []byte) (n int, err error) {
 	return
 }
 
-func (_ *HttpDownloadBackup) TrailerKey() string {
+func (b *HttpDownloadBackup) TrailerKey() string {
 	return http.CanonicalHeaderKey("X-Backup-Error")
 }
 
@@ -69,27 +69,27 @@ type StreamedWriter interface {
 	WriteStream(reader io.Reader) error
 }
 
-func (this *HttpDownloadBackup) DownloadBackup(backupURL string, backupWriter StreamedWriter) error {
-	this.logger.Info("Starting to take backup", lager.Data{
+func (b *HttpDownloadBackup) DownloadBackup(backupURL string, backupWriter StreamedWriter) error {
+	b.logger.Info("Starting to take backup", lager.Data{
 		"url": backupURL,
 	})
 
 	httpClient := &http.Client{
 		Transport: &http.Transport{
-			TLSClientConfig: this.config.TLS.Config,
+			TLSClientConfig: b.config.TLS.Config,
 		},
 	}
 
 	request, err := http.NewRequest("GET", backupURL, nil)
 	if err != nil {
-		this.logger.Error("Failed to create http request", err)
+		b.logger.Error("Failed to create http request", err)
 		return errors.WithStack(err)
 	}
 
-	request.SetBasicAuth(this.config.Credentials.Username, this.config.Credentials.Password)
+	request.SetBasicAuth(b.config.Credentials.Username, b.config.Credentials.Password)
 	resp, err := httpClient.Do(request)
 	if err != nil {
-		this.logger.Error("Failed to make http request", err)
+		b.logger.Error("Failed to make http request", err)
 		return errors.WithStack(err)
 	}
 
@@ -107,7 +107,7 @@ func (this *HttpDownloadBackup) DownloadBackup(backupURL string, backupWriter St
 			err = errors.New("Non-200 http Response")
 		}
 
-		this.logger.Error("Response returned non-200", err, lager.Data{
+		b.logger.Error("Response returned non-200", err, lager.Data{
 			"response status": resp.Status,
 		})
 		return err
@@ -117,7 +117,7 @@ func (this *HttpDownloadBackup) DownloadBackup(backupURL string, backupWriter St
 
 	copyErrChan := make(chan error)
 	go func() {
-		this.logger.Debug("Copying response body to backup writer")
+		b.logger.Debug("Copying response body to backup writer")
 		err = backupWriter.WriteStream(trackingReader)
 		copyErrChan <- err
 	}()
@@ -126,22 +126,22 @@ func (this *HttpDownloadBackup) DownloadBackup(backupURL string, backupWriter St
 	done := false
 	for done == false {
 		select {
-		case <-this.clock.After(1 * time.Minute):
-			this.logger.Info(fmt.Sprintf("Downloaded %s of backup so far", humanize.Bytes(uint64(trackingReader.getBytesRead()))))
+		case <-b.clock.After(1 * time.Minute):
+			b.logger.Info(fmt.Sprintf("Downloaded %s of backup so far", humanize.Bytes(uint64(trackingReader.getBytesRead()))))
 		case copyErr = <-copyErrChan:
 			done = true
 		}
 	}
 
 	if copyErr != nil {
-		this.logger.Error("Failed to copy response to writer", copyErr)
+		b.logger.Error("Failed to copy response to writer", copyErr)
 		return errors.WithStack(err)
 	}
 
-	errorMessage := resp.Trailer.Get(this.TrailerKey())
+	errorMessage := resp.Trailer.Get(b.TrailerKey())
 	if len(errorMessage) > 0 {
 		err := errors.New(errorMessage)
-		this.logger.Error("The download was incomplete", err)
+		b.logger.Error("The download was incomplete", err)
 		return err
 	}
 
