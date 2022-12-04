3011fc784850c58b490dcdcd9eee4e9ef6f84a4b
Andrew Garner <garnera@vmware.com>
2022-12-04 03:37:23 -0600

    e2e-tests: set working directory before running ginkgo
    Make it easier to run this script directly in CI without the consumer
    needing to first change to the src/e2e-tests directory.
    
    [#183599195](https://www.pivotaltracker.com/story/show/183599195)

--- src/e2e-tests/scripts/run.sh
+++ src/e2e-tests/scripts/run.sh
@@ -2,8 +2,12 @@
 
 set -o errexit -o nounset
 
+test_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." &>/dev/null && pwd)
+
 : "${MYSQL_ENGINE:="pxc-8.0"}"
 
 export MYSQL_ENGINE
 
+cd "${test_dir}"
 go run github.com/onsi/ginkgo/v2/ginkgo "$@"
+cd -
