9b5b200735491123bb385234b6149a88df82bcd5
Ryan Wittrup <rwittrup@vmware.com>
2023-12-14 12:22:36 -0500

    Removes unused bosh utilities
    [#186582745](https://www.pivotaltracker.com/story/show/186582745)
    
    Authored-by: Ryan Wittrup <rwittrup@vmware.com>

--- src/e2e-tests/utilities/bosh/bosh.go
+++ src/e2e-tests/utilities/bosh/bosh.go
@@ -4,8 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"os"
-	"path/filepath"
 	"sort"
 	"strings"
 
@@ -24,17 +22,6 @@ type Instance struct {
 	VMCid    string `json:"vm_cid"`
 }
 
-func CloudCheck(deploymentName string) error {
-	return cmd.Run(
-		"bosh",
-		"--deployment="+deploymentName,
-		"--non-interactive",
-		"--tty",
-		"cloud-check",
-		"--resolution=recreate_vm",
-	)
-}
-
 func DeleteDeployment(deploymentName string) error {
 	return cmd.Run(
 		"bosh",
@@ -43,113 +30,6 @@ func DeleteDeployment(deploymentName string) error {
 		"delete-deployment",
 		"--force",
 	)
-}
-
-func DeleteVM(deploymentName, cid string) error {
-	return cmd.Run(
-		"bosh",
-		"--deployment="+deploymentName,
-		"--tty",
-		"--non-interactive",
-		"delete-vm",
-		cid,
-	)
-}
-
-// Deploy run bosh deploy with an arbitrary manifest file and arguments
-//
-//	This function uses the default working directory
-func Deploy(deploymentName, manifestPath string, options ...DeployOptionFunc) error {
-	args := []string{
-		"--non-interactive",
-		"--tty",
-		"--deployment=" + deploymentName,
-		"deploy", manifestPath,
-	}
-
-	for _, o := range options {
-		o(&args)
-	}
-
-	return cmd.Run("bosh", args...)
-}
-
-// DeployPXC deploys the top-level pxc-deployment.yml manifest from the top-level of this pxc-release repo
-// This function sets the current working directory to the top-level of this repo
-func DeployPXC(deploymentName string, options ...DeployOptionFunc) error {
-	args := []string{
-		"--non-interactive",
-		"--tty",
-		"--deployment=" + deploymentName,
-		"deploy",
-		"pxc-deployment.yml",
-		"--ops-file=operations/deployment-name.yml",
-		"--var=deployment_name=" + deploymentName,
-	}
-
-	if pxcVersion := os.Getenv("PXC_DEPLOY_VERSION"); pxcVersion != "" {
-		args = append(args,
-			"--ops-file=operations/pxc-version.yml",
-			"--var=pxc_version="+pxcVersion,
-		)
-	}
-
-	for _, o := range options {
-		o(&args)
-	}
-
-	return cmd.RunCustom(cmd.WithCwd("../.."), "bosh", args...)
-}
-
-func RedeployPXC(deploymentName string, options ...DeployOptionFunc) error {
-	manifestFile, err := os.CreateTemp("", "pxc_manifest_")
-	if err != nil {
-		return fmt.Errorf("failed to create temporary file for bosh manifest: %w", err)
-	}
-	defer func(name string) {
-		_ = os.Remove(name)
-	}(manifestFile.Name())
-
-	if err = cmd.RunWithoutOutput(manifestFile, "bosh",
-		"--deployment="+deploymentName,
-		"manifest"); err != nil {
-		return fmt.Errorf("failed to retrieve bosh manifest: %w", err)
-	}
-
-	if err = manifestFile.Close(); err != nil {
-		return fmt.Errorf("failed to close manifest file: %w", err)
-	}
-
-	args := []string{
-		"--non-interactive",
-		"--deployment=" + deploymentName,
-		"deploy",
-		"--no-redact",
-		"--tty",
-		manifestFile.Name(),
-	}
-	for _, o := range options {
-		o(&args)
-	}
-
-	return cmd.RunCustom(cmd.WithCwd("../.."), "bosh", args...)
-}
-
-// Operation is a helper method to compute an ops-file path relative to the top-level ./operations directory
-func Operation(path string) DeployOptionFunc {
-	// This function assumes that bosh deploy is run with a cwd of the top of this repo.
-	// All standard ops-files will be relative to ./operations/
-	return func(args *[]string) {
-		*args = append(*args, "--ops-file="+filepath.Join("operations", path))
-	}
-}
-
-func Var(key, value string) DeployOptionFunc {
-	// This function assumes that bosh deploy is run with a cwd of the top of this repo.
-	// All standard ops-files will be relative to ./operations/
-	return func(args *[]string) {
-		*args = append(*args, "--var="+key+"="+value)
-	}
 }
 
 func Instances(deploymentName string, matchInstanceFunc MatchInstanceFunc) ([]Instance, error) {
@@ -213,38 +93,6 @@ func MatchByInstanceGroup(name string) MatchInstanceFunc {
 	}
 }
 
-// MatchByIndexedName matches by comparing the provided name to INSTANCE-GROUP/INDEX
-func MatchByIndexedName(name string) MatchInstanceFunc {
-	return func(i Instance) bool {
-		components := strings.SplitN(i.Instance, "/", 2)
-		instanceGroup := components[0]
-		return instanceGroup+"/"+i.Index == name
-	}
-}
-
-func RunErrand(deploymentName, errandName, instanceSpec string) error {
-	return cmd.Run(
-		"bosh",
-		"--deployment="+deploymentName,
-		"--non-interactive",
-		"--tty",
-		"run-errand",
-		errandName,
-		"--instance="+instanceSpec,
-	)
-}
-
-func Restart(deploymentName, instanceSpec string) error {
-	return cmd.Run(
-		"bosh",
-		"--deployment="+deploymentName,
-		"--non-interactive",
-		"--tty",
-		"restart",
-		instanceSpec,
-	)
-}
-
 func RemoteCommand(deploymentName, instanceSpec, cmdString string) (string, error) {
 	var output bytes.Buffer
 	err := cmd.RunWithoutOutput(&output,
@@ -259,36 +107,22 @@ func RemoteCommand(deploymentName, instanceSpec, cmdString string) (string, erro
 	return strings.TrimSpace(output.String()), err
 }
 
-func Logs(deploymentName, instanceSpec, filter string) (*bytes.Buffer, error) {
-	var output bytes.Buffer
-	err := cmd.RunWithoutOutput(&output,
-		"bosh",
-		"--deployment="+deploymentName,
-		"logs",
-		"--num=1024",
-		instanceSpec,
-		"--only="+filter,
-	)
-
-	return &output, err
-}
-
-func Scp(deploymentName, sourcePath, destPath string, args...string) error {
-	defaultArgs := []string{"--deployment="+deploymentName, "scp"}
+func Scp(deploymentName, sourcePath, destPath string, args ...string) error {
+	defaultArgs := []string{"--deployment=" + deploymentName, "scp"}
 	defaultArgs = append(defaultArgs, args...)
 	defaultArgs = append(defaultArgs, sourcePath)
 	defaultArgs = append(defaultArgs, destPath)
 	return cmd.Run("bosh", defaultArgs...)
 }
 
-func Stop(deploymentName,  instanceSpec string, args...string) error {
+func Stop(deploymentName, instanceSpec string, args ...string) error {
 	defaultArgs := []string{"-d", deploymentName, "stop"}
 	defaultArgs = append(defaultArgs, args...)
 	defaultArgs = append(defaultArgs, instanceSpec)
 	return cmd.Run("bosh", defaultArgs...)
 }
 
-func Start(deploymentName,  instanceSpec string, args...string) error {
+func Start(deploymentName, instanceSpec string, args ...string) error {
 	defaultArgs := []string{"-d", deploymentName, "start"}
 	defaultArgs = append(defaultArgs, args...)
 	defaultArgs = append(defaultArgs, instanceSpec)
